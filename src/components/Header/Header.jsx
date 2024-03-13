dca3037ca616468cdbdf9a12425c2e741c4a11d6
Maruf Hossain <mdmarufh974@gmail.com>
2024-03-13 14:34:17 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center mt-12'>
+            <h1 className='text-3xl'>Kowlede Cafe</h1>
+            <img src="../images/profile.png" alt="" />
+        </div>
+    );
+};
+
+export default Header;
